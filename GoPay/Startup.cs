3f48d97f2fccfca34eaed2afa670593e748a5ba6
Pavel Hornak <hornak.pavel@gmail.com>
2019-02-25 22:22:24 +0100

    gitignore added

--- GoPay/Startup.cs
+++ GoPay/Startup.cs
@@ -35,7 +35,7 @@ namespace GoPay
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "GoPay API", Version = "v1" });
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
@@ -65,7 +65,7 @@ namespace GoPay
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GoPay API V1");
             });
 
         }
